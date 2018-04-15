8632f349371a4a33447d0f80142c136723b76ac7
Devon Langendoerfer <425devon@gmail.com>
2018-04-15 15:57:34 -0700

    improved modualarization

--- Angular/quoterRank/server.js
+++ Angular/quoterRank/server.js
@@ -10,7 +10,6 @@ app.use(express.static(__dirname + '/public/dist'));
 require("./server/config/mongoose.js");
 require('./server/config/routes.js')(app);
 
-
 app.listen(8000, function () {
     console.log("listening on port 8000");
 })
